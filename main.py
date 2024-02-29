d976f6c2486000a9100a825b5352cc02682e0ab7
WillyMaldonado <maldonadoguillermo709@gmail.com>
2024-02-29 09:53:50 -0600

    just comment a line in code

--- main.py
+++ main.py
@@ -11,30 +11,30 @@ def main():
     b.insert_at_beginning(0)
     b.insert_at_beginning(4)
     b.insert_at_beginning(-17)
-    # while True:
-    #     user_choice = int(input("1) Add components to polynomial \n"
-    #                             "2) Add and subtract \n"
-    #                             "3) Evaluate \n"
-    #                             "4) Exit\n"))
-    #     if user_choice == 1:
-    #         polynomial_choice = input("Which polynomial you wanna add components? \n"
-    #                                     "'a)' or 'b)'\n")
-    #         if polynomial_choice == "a":
-    #             amount = int(input("How many components you wanna add?\n"))
-    #             for i in range(amount):
-    #                 component = int(input("Insert the component you wanna add \n"))
-    #                 a.insert_at_beginning(component)
-    #         else:
-    #             amount = int(input("How many components you wanna add?\n"))
-    #             for i in range(amount):
-    #                 component = int(input("Insert the component you wanna add \n"))
-    #                 b.insert_at_beginning(component)  
+    while True:
+        user_choice = int(input("1) Add components to polynomial \n"
+                                "2) Add and subtract \n"
+                                "3) Evaluate \n"
+                                "4) Exit\n"))
+        if user_choice == 1:
+            polynomial_choice = input("Which polynomial you wanna add components? \n"
+                                        "'a)' or 'b)'\n")
+            if polynomial_choice == "a":
+                amount = int(input("How many components you wanna add?\n"))
+                for i in range(amount):
+                    component = int(input("Insert the component you wanna add \n"))
+                    a.insert_at_beginning(component)
+            else:
+                amount = int(input("How many components you wanna add?\n"))
+                for i in range(amount):
+                    component = int(input("Insert the component you wanna add \n"))
+                    b.insert_at_beginning(component)  
     # #     elif user_choice == 2:
     # #         pass
     # #     elif user_choice == 3:
     # #         pass
-    #     elif user_choice == 4:
-    #         break
+        elif user_choice == 4:
+            break
     # # first = Circular_list()
     # # first.insert_at_beginning(-10)
     # # first.show_list()
@@ -44,6 +44,6 @@ def main():
     # # first.show_list()
     a.show_list()
     b.show_list()
-    a.evaluate_polynomial(3)
+    # a.evaluate_polynomial(3)
 
 main()
